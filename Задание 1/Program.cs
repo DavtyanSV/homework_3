df0c7476f653b7ab94776dec6ba10c22f3c6ea5e
Сурен Давтян <surendavtan@DavtyanS.local>
2023-04-02 20:32:08 +0300

    Выполнили дз для семинара

---
+++ Задание 1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+string ReadIn(string message)
+{
+    System.Console.WriteLine($"{message}");
+    string input = Console.ReadLine();
+    return input;
+}
+
+string Check (string number)
+{
+    
+    
+    if(number[0] == number[4] && number[1]==number[3])
+    {
+       return number = "Да";
+    }
+    else
+   return number="нет";
+}
+
+bool CorrectNumber(string number)
+{
+    if (number[4]!= null)
+    {
+        return true;
+    }
+    return false;
+}
+
+string numb = ReadIn("Введите число");
+
+
+if (CorrectNumber(numb))
+{
+    System.Console.WriteLine($"Является ли ваше число палиндромом? {Check(numb)} ");
+}
---
+++ Задание 2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine($"{message} -> ");
+    int something = int.Parse(Console.ReadLine());
+    return something;
+}
+int x1 = ReadInt("Введите x первой точки");
+int y1 = ReadInt("Введите y первой точки");
+int z1 = ReadInt("Введите z первой точки");
+
+int x2 = ReadInt("Введите x второй точки");
+int y2 = ReadInt("Введите y второй точки");
+int z2 = ReadInt("Введите z второй точки");
+
+double length = Math.Round(Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2)), 2); // Возводим внутри каждую разность в квадрот и позже из суммы разностей вычитаем корень. Также округляем вычисление до двух знаков после запятой
+
+System.Console.WriteLine($"Расстояние между двумя точками в 3д пространстве = {length}");
